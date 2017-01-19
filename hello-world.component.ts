be20d2bc8e917300e0d1642d2296fc9892f38474
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2017-01-19 20:33:21 +0900

    Add: add a hello-world

---
+++ hello-world.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'hello-world',
+    template: `
+        <h1>Hellow World!</h1>
+    `
+})
+export class HelloWorldComponent {}
